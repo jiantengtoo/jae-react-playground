a0b85de17ada97c24b0fd13a02603c5eb6ec8cf0
aiyo321 <aiyo321@gmail.com>
2020-04-21 11:40:52 +0800

    Updates

---
+++ precache-manifest.7aad9e649550f51249b2e13be3d76b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00473512a3a170289f06a83589875e1b",
+    "url": "/jae-react-playground/index.html"
+  },
+  {
+    "revision": "53487f9176398861277e",
+    "url": "/jae-react-playground/static/js/2.de88c0b4.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "/jae-react-playground/static/js/2.de88c0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cf0fae2c004f538795",
+    "url": "/jae-react-playground/static/js/main.21cf8e69.chunk.js"
+  },
+  {
+    "revision": "280a31f72272b1e7d6af",
+    "url": "/jae-react-playground/static/js/runtime-main.06e72ea8.js"
+  }
+]);
