768b5de35eeff397b7d946598b679dafe1a90458
aiyo321 <aiyo321@gmail.com>
2020-04-21 09:59:06 +0800

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import { Grid, Typography, Button, Popover, TextField } from '@material-ui/core';
-import Draggable from 'react-draggable';
+import { Grid, Button } from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Test from './components/Test';
 
 const styles = (theme: Theme) => ({
-  tf:{
-    maginLeft:"200px",
-  }
 });
 
 export interface ITemplateProps extends WithStyles {
@@ -15,43 +18,26 @@ export interface ITemplateProps extends WithStyles {
 }
 
 const App: React.FunctionComponent<ITemplateProps> = (props) => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
 
   return (
     <Grid>
-      <Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
-        Open Popover
-      </Button>
-      <Draggable>
-        <Popover
-          id={id}
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-        >
-          <Typography>The content of the Popover.</Typography> 
-        </Popover>
-      </Draggable>
-      <TextField className={props.classes.tf}/>
+      <Router>
+        <Link to="/">
+          <Button variant={'contained'}>
+            Main
+          </Button>
+        </Link>
+        <Link to="/test">
+          <Button variant={'contained'}>
+            Test
+          </Button>
+        </Link>
+        <Switch>
+          <Route path="/test">
+            <Test />
+          </Route>
+        </Switch>
+      </Router>
     </Grid>
   );
 }
